36c078fefdaa2add18b0d56e23003a57e3fe4138
Vi Đoàn <kenamhong@gmail.com>
2022-06-03 11:32:17 +0700

    keynamhong@gmail.com

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Em nói cho anh biết anh quên gì được không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra là em phải nhắn tin cho anh nhá :> ',
+    btnYes: 'Được 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi anh quên gì vậy?',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Anh xin lỗi, Anh nhớ em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/hqh2459'
+}
